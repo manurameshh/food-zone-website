6403ba35f4dfbf1c8fd775068504d2c73e363ff5
manurameshh <manuramesh988@gmail.com>
2026-05-15 03:24:46 +0530

    Initial project upload
    Uploaded responsive food delivery website project built using HTML, CSS, and JavaScript with sticky navigation, responsive layout, smooth scrolling, and modern UI design.

---
+++ script.js
@@ -0,0 +1,15 @@
+//for navbar
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+
+}
+
+//for search bar
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    
+}
